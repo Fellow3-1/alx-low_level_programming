4097ba5fe4313d07be8fc1dbc9f4f399f4dadc76
Felloh31 <felixodhiambo31@live.com>
2022-06-04 02:00:48 +0300

    ++betty fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
-
 /**
-* main - Print combinations of two digit numbers
-*
-* Return: Always 0 (Success)
-*/
+ * main - print numbers from 00 to 99.
+(*
+ * Return: 0 on success
+ */
 int main(void)
 {
-  int tens;
-  int ones;
+	int i = '0';
+	int j = '0';
 
-  for (tens = 0; tens <= 9; tens++)
-    {
-      for (ones = tens + 1; ones <= 9; ones++)
+	while (i <= '9')
 	{
-	  putchar(tens + '0');
-	  putchar(ones + '0');
-
-	  if (tens < 8)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
+		while (j <= '9')
+		{
+			putchar(i);
+			putchar(j);
+			if (j == '9' && i == '9')
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		if (j >= '9')
+		{
+			j = '0';
+		}
+		i++;
 	}
-    }
-  putchar('\n');
-
-  return (0);
+	return (0);
 }
+
+
