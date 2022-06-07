21adf8a6d510b1c9df4e37f5b65c6fc4c50693b2
Felloh31 <felixodhiambo31@live.com>
2022-06-08 01:52:16 +0300

    ++betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 - print alphabet 10 times
- * 
- * Return: void
- */
-
+* print_alphabet_x10 - Prints the alphabet 10 times.
+*
+* Return: Always 0
+*/
 void print_alphabet_x10(void)
 {
 	int i;
