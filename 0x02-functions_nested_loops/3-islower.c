8b3074bd834c36de9369669b9a34f8233fabffc3
Felloh31 <felixodhiambo31@live.com>
2022-06-07 21:08:49 +0300

    ++mambaru

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,13 +9,8 @@
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-	return (1);
-	}
+	if (c >= 'a' && c <= 'z')
+		return (1);
 	else
-	{
-	return (0);
-	}
-	_putchar('\n');
+		return (0);
 }
