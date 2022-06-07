99a55c491c0f6b3e9393e14d6fe7db8ff52631e8
Felloh31 <felixodhiambo31@live.com>
2022-06-07 19:12:19 +0300

    ++Putchar & Alph

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main()
+{
+
+	// Character to be written
+	char ch = '0';
+
+	// Character to stdout
+	putchar(ch);
+
+	return (0);
+}
