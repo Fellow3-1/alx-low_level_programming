8801caa69e5f7edfff1a3de9eb1007ec9e0427a5
Felloh31 <felixodhiambo31@live.com>
2022-06-20 13:58:05 +0300

    ++7fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints an 8x8 chessboard
- * @a: 2d char array representing board
+ * print_chessboard - prints the chessboard
+ * @a: pointer to pieces to print
  *
  * Return: void
  */
 void print_chessboard(char (*a)[8])
-@@ -13,7 +14,7 @@ void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[j][j]);
-			_putchar((*(a + i))[j]);
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
+}
